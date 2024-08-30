27618c480f487571307555bc2bd7a658713d531d
dinesh <dinesh-rock>
2024-08-30 15:48:48 +0530

    udated files on test

--- test.php
+++ test.php
@@ -4,5 +4,8 @@
 <?php echo "demo123"; ?>
 <?php echo "demo12213"; ?>
 <?php echo "testing revert"; ?>
+<<<<<<< HEAD
 <?php echo "good Morning"; ?>
 <?php echo "good Morning"; ?>
+=======
+>>>>>>> parent of 5c0220a... good morning 1
